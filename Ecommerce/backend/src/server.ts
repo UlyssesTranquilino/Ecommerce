ef74313c7eeb7975db082e8f67125b49ef54ca3c
Ulysses Tranquilino <129757717+UlyssesTranquilino@users.noreply.github.com>
2025-01-26 20:27:27 +0800

    use this

--- Ecommerce/backend/src/server.ts
+++ Ecommerce/backend/src/server.ts
@@ -1,6 +1,3 @@
-//TUTORIAL: https://medium.com/@induwara99/a-step-by-step-guide-to-setting-up-a-node-js-project-with-typescript-6df4481cb335
-
-// Import the 'express' module along with 'Request' and 'Response' types from express
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
@@ -17,32 +14,12 @@ const _dirname = path.resolve();
 // Create an Express application
 const app = express();
 
-// Specify the allowed origins
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://192.168.56.1:5173",
-  "http://192.168.1.9:5173",
-  "http://192.168.1.5:5173",
-  "http://192.168.1.6:5173",
-  "http://192.168.1.7:5173",
-  "https://shop-at-exclusive.netlify.app/",
-];
-
-// Configure CORS middleware
+// Configure CORS middleware to allow all origins
 const corsOptions = {
-  origin: (
-    origin: string | undefined,
-    callback: (err: Error | null, allow?: boolean) => void
-  ) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*", // Allow all origins
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); // Apply CORS middleware globally
 app.use(express.json());
 app.use("", userRouter);
 app.use("", router);
